b9efe835294bd9bbbd7eafeb8522ee8cc4f0f3e9
Carl E. Ma <enhong.ma@autodesk.com>
2022-08-05 21:57:33 -0700

    failure module7

--- module7/module7.go
+++ module7/module7.go
@@ -1,3 +1,5 @@
 package module7
 
 //go:generate echo "command and arguments"
+//go:generate /Users/maen/go/bin/goimports github.com/golang/go/src/cmd/go/internal/fmtcmd/fmt.go
+//go:generate /Users/maen/go/bin/goimports runtime
