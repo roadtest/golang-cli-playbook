4a4f6c591515d4ad28efd79541c6a3a49b158807
Carl E. Ma <enhong.ma@autodesk.com>
2022-08-05 22:02:00 -0700

    module7

--- module7/module7.go
+++ module7/module7.go
@@ -1,5 +1,4 @@
 package module7
 
 //go:generate echo "command and arguments"
-//go:generate /Users/maen/go/bin/goimports github.com/golang/go/src/cmd/go/internal/fmtcmd/fmt.go
-//go:generate /Users/maen/go/bin/goimports runtime
+//go:generate goimports -w module7_code.go
